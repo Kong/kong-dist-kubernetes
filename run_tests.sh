12a0ef1281a321280a68d083e82599b126f5d41a
Colin Hutchinson <hutchic@users.noreply.github.com>
2019-09-24 16:42:04 -0400

    fix(control plane) run the Kong control plane as a ClusterIP instead of NodePort (#94)
    * fix(controlplane) switch control plane to a ClusterIP so it's not exposed by default
    
    * chore(control plane) change control plane to ClusterIP

--- run_tests.sh
+++ run_tests.sh
@@ -57,13 +57,12 @@ done
 
 KONG_VERSION=$(kubectl exec -n kong -it $(kubectl get pods -n kong | grep kong | head -n 1 | awk '{print $1}') -- kong version | tr -d '[:space:]')
 
+kubectl port-forward -n kong deployment/kong-control-plane 8001 &
 HOST="$(kubectl get nodes --namespace kong -o jsonpath='{.items[0].status.addresses[0].address}')"
 echo $HOST
-ADMIN_PORT=$(kubectl get svc --namespace kong kong-control-plane -o jsonpath='{.spec.ports[0].nodePort}')
-echo $ADMIN_PORT
 PROXY_PORT=$(kubectl get svc --namespace kong kong-ingress-data-plane -o jsonpath='{.spec.ports[0].nodePort}')
 echo $PROXY_PORT
 
 pushd kong-build-tools
-    TEST_ADMIN_URI=http://$HOST:$ADMIN_PORT TEST_PROXY_URI=http://$HOST:$PROXY_PORT KONG_VERSION=$KONG_VERSION make -f Makefile run_tests
+    TEST_ADMIN_URI=http://localhost:8001 TEST_PROXY_URI=http://$HOST:$PROXY_PORT KONG_VERSION=$KONG_VERSION make -f Makefile run_tests
 popd
